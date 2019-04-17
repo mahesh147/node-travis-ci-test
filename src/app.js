1eebf63ca5d7e19f493aa710f9ddb3ed816c6a8c
Mahesh Chandran <maheshchandran147@gmail.com>
2019-04-17 17:01:41 +0530

    Added Heroku configs

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
-
+const port = process.env.PORT || 3000;
 const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
@@ -104,6 +104,6 @@ app.get("*", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is up on port 3000");
 });
